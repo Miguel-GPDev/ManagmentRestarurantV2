d2b30c5431dff89419544dd2b1bc34d459ef3263
Miguel-GPDev <94927103+Miguel-GPDev@users.noreply.github.com>
2022-05-25 12:09:48 +0200

    Update Supplier_Adapter.java

--- app/src/main/java/com/example/managmentrestarurantv2/view/adapters/Supplier_Adapter.java
+++ app/src/main/java/com/example/managmentrestarurantv2/view/adapters/Supplier_Adapter.java
@@ -6,9 +6,38 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 import com.example.res.R;
-import com.example.res.rest.business.model.SupplierRestaurant;
+import com.example.res.rest.business.model.Booking;
 
 import java.util.List;
+
+public class Booking_Adapter extends RecyclerView.Adapter<Booking_Adapter.bookingHolder> {
+    List<Booking> bookingList;
+    @NonNull
+    @Override
+    public Booking_Adapter.bookingHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.view_booking, parent, false);
+        Booking_Adapter.bookingHolder holder = new bookingHolder(v);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull Booking_Adapter.bookingHolder holder, int position) {
+        Booking booking = bookingList.get(position);
+    }
+
+    @Override
+    public int getItemCount() {
+        return bookingList.size();
+    }
+
+    public class bookingHolder extends RecyclerView.ViewHolder{
+        //TODO
+        public bookingHolder(@NonNull View itemView) {
+            super(itemView);
+            //TODO
+        }
+    }
+}
 
 public class Supplier_Adapter extends RecyclerView.Adapter<Supplier_Adapter.supplierHolder> {
     List<SupplierRestaurant> supplierRestaurantList;
