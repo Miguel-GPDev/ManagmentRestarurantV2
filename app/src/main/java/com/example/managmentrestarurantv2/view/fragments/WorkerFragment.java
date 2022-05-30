739d8f80b3746acf91c8d50c8164815d6c60693f
Miguel-GPDev <94927103+Miguel-GPDev@users.noreply.github.com>
2022-05-30 15:51:45 +0200

    Update WorkerFragment.java

--- app/src/main/java/com/example/managmentrestarurantv2/view/fragments/WorkerFragment.java
+++ app/src/main/java/com/example/managmentrestarurantv2/view/fragments/WorkerFragment.java
@@ -9,7 +9,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import com.example.res.rest.business.model.Worker;
+import com.example.managmentrestarurantv2.business.model.Worker;
 import com.google.firebase.auth.FirebaseAuth;
 
 /**
