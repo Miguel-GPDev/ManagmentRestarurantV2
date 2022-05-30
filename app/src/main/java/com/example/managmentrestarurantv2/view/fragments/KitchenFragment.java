8eadd0ba8777391bdaa091c430773e1d782acc3b
Miguel-GPDev <94927103+Miguel-GPDev@users.noreply.github.com>
2022-05-30 16:17:00 +0200

    Update KitchenFragment.java

--- app/src/main/java/com/example/managmentrestarurantv2/view/fragments/KitchenFragment.java
+++ app/src/main/java/com/example/managmentrestarurantv2/view/fragments/KitchenFragment.java
@@ -12,7 +12,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import com.example.res.R;
-import com.example.res.rest.business.model.Kitchen;
+import com.example.managmentrestarurantv2.business.model.Kitchen;
 
 /**
  * A simple {@link Fragment} subclass.
